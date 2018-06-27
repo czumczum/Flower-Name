b48dd246ebb6669287a1a8b142f6acf12d7455fd
Ula <slodkosci@gmail.com>
2018-06-27 14:19:32 -0400

    Add pick a photo function

--- Flower Name/ViewController.swift
+++ Flower Name/ViewController.swift
@@ -8,18 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageViewController: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary //TODO: change to .camera on device
+        imagePicker.allowsEditing = false
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            
+            imageViewController.image = image
+            imagePicker.dismiss(animated: true, completion: nil)
+            
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func cameraButtonClicked(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
     }
-
-
+    
 }
 
